f5d5e35acaf4085f00a142c07f367b379ff61433
Aether Kerstens <philippekerstens@gmail.com>
2022-10-12 08:49:23 -0700

    Start iteration 2 and pass add pets method

--- spec/shelter_spec.rb
+++ spec/shelter_spec.rb
@@ -29,8 +29,6 @@ RSpec.describe Shelter do
   # Iteration 2
   describe '#add_pet' do
     it 'returns a list of pets' do
-      skip
-
       shelter = Shelter.new('Denver Animal Shelter', 5)
       shelter.add_pet('Salem')
       shelter.add_pet('Beethoven')
